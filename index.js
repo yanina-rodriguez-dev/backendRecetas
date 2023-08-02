d966871d85263a70885ecfe0fa1e71a9c7644191
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-08-01 22:43:06 -0300

    Agregamos un puerto

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import express from 'express';
-console.log('qua prueba backend');
+
+//puertos 
+const app = express();
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'),()=>{
+    console.log('ESTO EN EL PUERTO ' +app.get('port'))
+})
+
+
+
+//middelwares
+
+
+
+//routes
